0c3f5c01cd0db54fa9730a94e910c85665e58227
Shammya Saha <shammya.saha@asu.edu>
2025-07-10 23:38:23 -0400

    Redis installation added

--- install.sh
+++ install.sh
@@ -14,3 +14,14 @@ sudo echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 sudo . ~/.bashrc
 sudo apt-get install -y unixodbc-dev
 sudo apt-get install -y libgssapi-krb5-2
+
+
+sudo apt-get install lsb-release curl gpg
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis
+
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
